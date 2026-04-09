b7dc907d46b46f88a8fe74d0da030aeeec930dd9
aditya-m-mishra <adiapplies@gmail.com>
2026-04-09 13:56:40 +0530

    initial position for snake defined

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("khaki")
 screen.title("Snake Game")
 
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.goto(position)
 
 screen.exitonclick()
