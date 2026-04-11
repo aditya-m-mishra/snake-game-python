9b3bf7cf9c1a008bb47675bbc6a4b3d02a4f0d10
aditya-m-mishra <adiapplies@gmail.com>
2026-04-11 17:18:19 +0530

    Added highscores and reset function

--- main.py
+++ main.py
@@ -43,16 +43,14 @@ while game_is_on:
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
 
     # detect collision with snake's own body
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
 
 
 screen.exitonclick()
